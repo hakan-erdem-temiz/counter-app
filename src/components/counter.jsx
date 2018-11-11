fa6f7aed47fe5bcafa0644e6dab18a842bc371b6
hakan erdem <hkn.het@gmail.com>
2018-11-11 11:53:48 +0300

    counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,39 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold"
+  };
+
   render() {
     return (
-      <React.Fragment>
-        <h1>Hello Hakan :)</h1>
-        <button>Increment</button>
-      </React.Fragment>
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondry btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
